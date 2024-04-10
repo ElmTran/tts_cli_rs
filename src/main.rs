a6ec44a0a324b77e4ab826dc2db99ca481fe0c07
chenmengqing <chenmengqing@jiayinfintech.cn>
2024-04-10 20:04:38 +0800

    update: get_args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod tts;
 mod utils;
 
-// use clap::{Arg, ArgAction, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 use dotenv::dotenv;
 use std::fs::File;
 use std::io::Write;
@@ -12,70 +12,87 @@ use utils::time_util;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    // let matches = Command::new("tts-cli")
-    //     .about("Text to speech command line interface")
-    //     .version("0.0.1")
-    //     .subcommand_required(true)
-    //     .arg_required_else_help(true)
-    //     .subcommand(
-    //         Command::new("speak")
-    //             .short_flag('S')
-    //             .long_flag("speak")
-    //             .about("Speak text")
-    //             .arg(
-    //                 Arg::new("text")
-    //                     .short('t')
-    //                     .long("text")
-    //                     .help("Text to speak")
-    //                     .action(ArgAction::Set)
-    //                     .num_args(1..),
-    //             )
-    //             .arg(
-    //                 Arg::new("language")
-    //                     .short('l')
-    //                     .long("language")
-    //                     .help("Language to speak")
-    //                     .action(ArgAction::Set)
-    //                     .num_args(1..),
-    //             )
-    //             .arg(
-    //                 Arg::new("speaker")
-    //                     .short('s')
-    //                     .long("speaker")
-    //                     .help("Speaker to speak")
-    //                     .action(ArgAction::Set)
-    //                     .num_args(1..),
-    //             )
-    //             .arg(
-    //                 Arg::new("rate")
-    //                     .short('r')
-    //                     .long("rate")
-    //                     .help("Rate to speak")
-    //                     .action(ArgAction::Set)
-    //                     .num_args(1..),
-    //             ),
-    //     )
-    //     .get_matches();
-
-    // match matches.subcommand() {
-    //     Some(("speak", speak_matches)) => {
-    //         if speak_matches.contains_id("text") {
-    //             let text: &String = speak_matches.get_one::<String>("text").unwrap();
-    //             println!("Text: {}", text);
-    //         }
-    //     }
-    //     _ => (),
-    // }
+    let args = get_args();
     let params = param::Params::new(
-        "Hello, world!".to_string(),
-        "en-US-AriaNeural".to_string(),
-        "en-US".to_string(),
-        "chat".to_string(),
-        "0%".to_string(),
-        "0%".to_string(),
+        // "Hello, world!".to_string(),
+        // "en-US-AriaNeural".to_string(),
+        // "en-US".to_string(),
+        // "chat".to_string(),
+        // "0%".to_string(),
+        // "0%".to_string(),
+        args.get_one::<String>("text").unwrap().clone(),
+        args.get_one::<String>("speaker").unwrap().clone(),
+        args.get_one::<String>("language").unwrap().clone(),
+        args.get_one::<String>("style").unwrap().clone(),
+        args.get_one::<String>("rate").unwrap().clone(),
+        args.get_one::<String>("pitch").unwrap().clone(),
     );
     let res = azure::request(&params).await.unwrap();
     save_audio(&res).unwrap();
+}
+
+fn get_args() -> ArgMatches {
+    let matches = Command::new("tts-cli")
+        .about("Text to speech command line interface")
+        .version("0.0.1")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("speak")
+                .short_flag('S')
+                .long_flag("speak")
+                .about("Speak text")
+                .arg(
+                    Arg::new("text")
+                        .short('t')
+                        .long("text")
+                        .help("Text to speak")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                )
+                .arg(
+                    Arg::new("language")
+                        .short('l')
+                        .long("language")
+                        .help("Language to speak")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                )
+                .arg(
+                    Arg::new("style")
+                        .short('y')
+                        .long("style")
+                        .help("Style to speak")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                )
+                .arg(
+                    Arg::new("speaker")
+                        .short('s')
+                        .long("speaker")
+                        .help("Speaker to speak")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                )
+                .arg(
+                    Arg::new("rate")
+                        .short('r')
+                        .long("rate")
+                        .help("Rate to speak")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                )
+                .arg(
+                    Arg::new("pitch")
+                        .short('p')
+                        .long("pitch")
+                        .help("Pitch to speak")
+                        .action(ArgAction::Set)
+                        .num_args(1..),
+                ),
+        )
+        .get_matches();
+    matches
 }
 
 fn save_audio(audio: &[u8]) -> std::io::Result<()> {
