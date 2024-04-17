675396ed76f40c19db154b5fe6d3bec32eab0748
elmtran <c897611977@gmail.com>
2024-04-17 22:46:41 +0800

    chore: optimize main

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,9 @@
 use std::error::Error;
 use structopt::StructOpt;
+use crate::config;
+use crate::tts::param;
+use crate::tts::azure;
+use crate::utils::audio_util;
 
 #[derive(StructOpt)]
 #[structopt(name = "tts_cli_rs")]
@@ -45,4 +49,46 @@ fn parse_key_val<T>(s: &str) -> Result<[T; 2], Box<dyn Error>>
 {
     let parts = s.find('=').ok_or_else(|| format!("invalid KEY=value: no `=` found in `{}`", s))?;
     Ok([s[..parts].parse()?, s[parts + 1..].parse()?])
+}
+
+pub fn handle_command(cmd: &Command, config: &mut config::Config) {
+    match cmd {
+        Command::Config(arg) => {
+            if let Some(conf) = &arg.set {
+                if let Err(_) = config.set_config(&conf) {
+                    println!("Setting config failed, please check your input");
+                }
+            } else if let Some(query) = &arg.get {
+                let val = config.get_config(&query);
+                match val {
+                    Ok(v) => println!("{}", v),
+                    Err(e) => println!("{}", e),
+                }
+            }
+        }
+    }
+}
+
+pub async fn handle_option(opt: Opt, config: &config::Config) {
+    let params = param::Params::new(
+        opt.text.unwrap_or("please provide text to speak".to_string()),
+        opt.speaker,
+        opt.language,
+        opt.style,
+        opt.rate,
+        opt.pitch
+    );
+    let res = azure::speak(&params, &config).await;
+    match res {
+        Ok(_) => {
+            let audio = res.unwrap();
+            if let Some(output) = opt.output {
+                audio_util::write(&audio, &output).unwrap();
+            }
+            if let Err(_) = audio_util::play(&audio).await {
+                println!("Playing audio failed, please check your device");
+            }
+        }
+        Err(e) => println!("{}", e),
+    }
 }
